3708fe1788bcf7aed5afac22e7111a6bc5cadf27
Uzair0j0 <itsuzairop@gmail.com>
2026-06-24 13:22:21 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        double age = 21.5;
+        System.out.println(age);
+
+        int Age = 25;
+        int price = 12;
+
+        double Price = 12.99;
+
+        boolean isStudent = false;
+        if(isStudent){
+            System.out.println("You are a Student");
+        }
+        else {
+            System.out.println("You are Not a student");
+        }
+        char grade = 'A';
+        System.out.println("Your gradde is " + grade);
+
+    }
+}
