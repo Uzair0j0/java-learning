b37c30ad6a54b81e4e0c580572e80284916c44fe
Uzair0j0 <itsuzairop@gmail.com>
2026-07-15 15:17:20 +0500

    methods

---
+++ src/methods.java
@@ -0,0 +1,10 @@
+public class methods {
+    public static void main(String[] args){
+
+        int realage = 12;
+        age(realage);
+    }
+    static void age(int realage){
+        System.out.printf("You are %d years old", realage);
+    }
+}
